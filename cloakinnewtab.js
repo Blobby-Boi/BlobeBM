ce78e1ba9dc289d92189c199c81d896ddd28527d
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2023-11-16 13:44:52 -0800

    Update cloakinnewtab.js

--- cloakinnewtab.js
+++ cloakinnewtab.js
@@ -1,1 +1,2 @@
 (function(){var url = "https://8752ee92-3fcc-4eda-b1a2-7728cde6218b.id.firewalledreplit.co/"; var win = window.open(); var iframe = win.document.createElement('iframe'); iframe.style="position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:2147483647;background-color:white;border:none;"; if(url.includes('https://') || url.includes("http://")) {iframe.src = url;}else{iframe.src = "https://" + url;} win.document.body.appendChild(iframe);})();
+window.close()
