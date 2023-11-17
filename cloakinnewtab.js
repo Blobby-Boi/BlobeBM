66aa49c0ef61fb591ecc9fc9e3f74ebaf1a19563
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2023-11-17 15:46:05 -0800

    Update cloakinnewtab.js

--- cloakinnewtab.js
+++ cloakinnewtab.js
@@ -1,2 +1,31 @@
-(function(){var url = "https://8752ee92-3fcc-4eda-b1a2-7728cde6218b.id.firewalledreplit.co/"; var win = window.open(); var iframe = win.document.createElement('iframe'); iframe.style="position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:2147483647;background-color:white;border:none;"; if(url.includes('https://') || url.includes("http://")) {iframe.src = url;}else{iframe.src = "https://" + url;} win.document.body.appendChild(iframe);})();
-window.location.href = "https://google.com";
+(function () {
+    var url = "https://8752ee92-3fcc-4eda-b1a2-7728cde6218b.id.firewalledreplit.co/";
+
+    // Open a new window without specifying the name initially
+    var win = window.open();
+
+    // Set the name of the window to "test"
+    win.name = "test";
+
+    // Set the icon for the window
+    win.document.head.innerHTML += '<link rel="icon" href="lexialogo.png" type="image/png">';
+
+    // Set the title of the window
+    win.document.head.innerHTML += '<title>Lexia Powerup!</title>';
+
+    // Create an iframe
+    var iframe = win.document.createElement('iframe');
+
+    // Set styles for the iframe
+    iframe.style = "position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:2147483647;background-color:white;border:none;";
+
+    // Check and assign the URL to the iframe
+    if (url.includes('https://') || url.includes("http://")) {
+        iframe.src = url;
+    } else {
+        iframe.src = "https://" + url;
+    }
+
+    // Append the iframe to the body of the window
+    win.document.body.appendChild(iframe);
+})();
